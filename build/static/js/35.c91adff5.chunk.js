c72df7dfb0c9d3749055bb5e0f255e6d4aabdc84
Vikas sharma <vikas@sovtechnologies.com>
2022-12-27 20:46:38 +0530

    init

---
+++ build/static/js/35.c91adff5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[35],{432:function(p,s){}}]);
+//# sourceMappingURL=35.c91adff5.chunk.js.map
---
+++ build/static/js/5.26bc940b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[5],{487:function(p,n){},493:function(p,n){},495:function(p,n){}}]);
+//# sourceMappingURL=5.26bc940b.chunk.js.map
